a6a4cd5c5b530a59e4adde137e03bd772cc5aa03
Mariana-Teixeira <teixeira.marianareis@gmail.com>
2025-01-05 17:36:19 +0000

    refactor: clean delivery class

--- Runtime/Objects/Delivery.cs
+++ Runtime/Objects/Delivery.cs
@@ -1,26 +1,11 @@
-using UnityEngine;
-
 namespace CookingSystem
 {
      public class Delivery
      {
          private RecipeSO _requestedRecipeSO;
 
-         public void Deliver(Dish deliveredDish)
-         {
-             var delivery = Loader.Compare(deliveredDish, _requestedRecipeSO);
-             Debug.Log($"Delivery: {delivery}");
-         }
-         
-         private void RequestRandom()
-         {
-             _requestedRecipeSO = Loader.GetRandomRecipe();
-             Debug.Log(_requestedRecipeSO.name);
-         }
-
-         public void Request(RecipeSO requestRecipe)
-         {
-             _requestedRecipeSO = requestRecipe;
-         }
+         public bool Deliver(Dish deliveredDish) => Loader.Compare(deliveredDish, _requestedRecipeSO);
+         private void RequestRandom() => _requestedRecipeSO = Loader.GetRandomRecipe();
+         public void Request(RecipeSO requestRecipe) => _requestedRecipeSO = requestRecipe;
      }
 }
